520e246c538307f6c10cd459892b416864993c70
developerazadpro <azharul.ece13.hstu@gmail.com>
2022-12-13 23:58:06 +0600

    string problem # 709 to lower case

---
+++ string/problem_709.php
@@ -0,0 +1,13 @@
+<?php
+    # To Lower Case
+    class Solution {
+        function toLowerCase($s) {
+            echo strtolower($s);
+        }
+    }
+    $solution = new Solution();
+    $solution->toLowerCase('Hello');
+    //$solution->toLowerCase('here');
+    //$solution->toLowerCase('LOVELY');
+
+?>
