a84681ed72dfcc5f7b290abeb336fcc926825c72
developerazadpro <azharul.ece13.hstu@gmail.com>
2022-12-19 23:22:31 +0600

    problem 412 - fizzbuzz

---
+++ string/fizzbuzz_412.php
@@ -0,0 +1,33 @@
+<?php
+    class Solution {
+
+        /**
+         * @param Integer $n
+         * @return String[]
+         */
+        function fizzBuzz($n) {
+            $res = [];
+            for($i = 1; $i <= $n; $i++){
+                if($i % 3 == 0 && $i % 5 == 0){
+                    $str = 'FizzBuzz';
+                    array_push($res, $str);
+                }else if($i % 3 == 0 ){
+                    $str = 'Fizz';;
+                    array_push($res, $str);
+                }else if($i % 5 == 0){
+                    $str = 'Buzz';;
+                    array_push($res, $str);
+                }else{
+                    $str = (string)$i;;
+                    array_push($res, $str);
+                }
+            }
+            return $res;
+        }
+    }
+
+    $sol = new Solution();
+    echo '<pre>';print_r($sol->fizzBuzz(15));
+    
+
+?>
