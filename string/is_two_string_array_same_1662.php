80380fac012e3fd21a5c23a294d52044d3c68632
developerazadpro <azharul.ece13.hstu@gmail.com>
2022-12-21 23:34:08 +0600

    is two string array same p # 1662

---
+++ string/is_two_string_array_same_1662.php
@@ -0,0 +1,23 @@
+<?php
+    class Solution {
+
+        /**
+         * @param String[] $word1
+         * @param String[] $word2
+         * @return Boolean
+         */
+        function arrayStringsAreEqual($word1, $word2) {
+            $word1 = implode('', $word1);
+            $word2 = implode('', $word2);
+            return ($word1 == $word2 ? true : false);
+        }
+
+        
+    }
+
+    $word1 = ["ab", "c"];
+    $word2 = ["a", "bc"];
+    $sol = new Solution();
+    $sol->arrayStringsAreEqual($word1, $word2);
+
+?>
